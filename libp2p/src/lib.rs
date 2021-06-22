d8190f6959553be9492961f8bfb9f88494bf3c7f
David Craven <david@craven.ch>
2021-06-22 16:56:17 +0200

    Networking part 3.

--- libp2p/src/lib.rs
+++ libp2p/src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! ## Local streams
 //!
-//! ```rust
+//! ```ignore
 //! # use anyhow::Result;
 //! # fn main() -> Result<()> {
 //! # let streams = StreamSync::open(path, key)?;
@@ -18,7 +18,7 @@
 //!
 //! ## Replicated streams
 //!
-//! ```rust
+//! ```ignore
 //! # use anyhow::Result;
 //! # fn main() -> Result<()> {
 //! # let streams = StreamSync::open(path, key)?;
@@ -40,7 +40,7 @@
 //!
 //! ## Startup
 //!
-//! ```rust
+//! ```ignore
 //! # use anyhow::Result;
 //! # fn main() -> Result<()> {
 //! # let streams = StreamSync::open(path, key)?;
@@ -124,6 +124,7 @@ pub struct StreamSyncRequest {
     len: u64,
 }
 
+#[derive(Debug)]
 pub enum StreamSyncEvent {
     NewHead(Head),
 }
@@ -247,6 +248,11 @@ impl StreamSync {
     }
 
     fn start_sync(&mut self, head: SignedHead) {
+        tracing::info!(
+            "starting sync {} to {}",
+            head.head().id(),
+            head.head().len()
+        );
         let stream = if let Some(stream) = self.streams.get_mut(head.head().id()) {
             stream
         } else {
@@ -267,6 +273,7 @@ impl StreamSync {
     }
 
     fn request_slice(&mut self, state: RequestState) {
+        tracing::info!("requesting slice");
         let stream = self.streams.get(&state.stream).unwrap();
         let peer = stream.peers[(state.slice_index + state.peer_index) % stream.peers.len()];
         let slice = &stream.buffer.slices()[state.slice_index];
@@ -280,12 +287,14 @@ impl StreamSync {
     }
 
     fn send_slice(&mut self, ch: ChannelId, id: &StreamId, start: u64, len: u64) -> Result<()> {
+        tracing::info!("sending slice");
         self.store.extract(id, start, len, &mut self.slice)?;
         self.inner.respond_final(ch, self.slice.clone())?;
         Ok(())
     }
 
     fn recv_slice(&mut self, req: RequestId, _no: SequenceNo, slice: Slice) {
+        tracing::info!("received slice");
         let stream = if let Some(stream) = self.streams.get_mut(slice.head.head().id()) {
             stream
         } else {
@@ -429,5 +438,126 @@ impl NetworkBehaviour for StreamSync {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+    use blake_streams::{PublicKey, SecretKey};
+    use futures::stream::StreamExt;
+    use libp2p::core::muxing::StreamMuxerBox;
+    use libp2p::core::transport::{Boxed, MemoryTransport, Transport};
+    use libp2p::core::upgrade::Version;
+    use libp2p::identity;
+    use libp2p::plaintext::PlainText2Config;
+    use libp2p::swarm::{Swarm, SwarmEvent};
+    use libp2p::yamux::YamuxConfig;
+    use rand::RngCore;
+    use std::io::Read;
+    use std::time::Duration;
+    use tempdir::TempDir;
+
+    fn tracing_try_init() {
+        tracing_subscriber::fmt()
+            .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+            .try_init()
+            .ok();
+    }
+
+    fn rand_bytes(size: usize) -> Vec<u8> {
+        let mut rng = rand::thread_rng();
+        let mut data = Vec::with_capacity(size);
+        data.resize(data.capacity(), 0);
+        rng.fill_bytes(&mut data);
+        data
+    }
+
+    fn keypair(secret: [u8; 32]) -> Keypair {
+        let secret = SecretKey::from_bytes(&secret).unwrap();
+        let public = PublicKey::from(&secret);
+        Keypair { secret, public }
+    }
+
+    fn build_dev_transport(
+        key_pair: identity::Keypair,
+    ) -> anyhow::Result<Boxed<(PeerId, StreamMuxerBox)>> {
+        let plaintext_config = PlainText2Config {
+            local_public_key: key_pair.public(),
+        };
+        let yamux_config = YamuxConfig::default();
+        let transport = MemoryTransport {}
+            .upgrade(Version::V1)
+            .authenticate(plaintext_config)
+            .multiplex(yamux_config)
+            .timeout(Duration::from_secs(10))
+            .map(|(peer_id, muxer), _| (peer_id, StreamMuxerBox::new(muxer)))
+            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))
+            .boxed();
+        Ok(transport)
+    }
+
+    fn build_swarm(
+        path: &Path,
+        mut secret: [u8; 32],
+        slice_len: usize,
+    ) -> Result<Swarm<StreamSync>> {
+        let behaviour = StreamSync::open(path, keypair(secret), slice_len)?;
+        let secret = identity::ed25519::SecretKey::from_bytes(&mut secret)?;
+        let key = identity::Keypair::Ed25519(secret.into());
+        let peer_id = key.public().into_peer_id();
+        let transport = build_dev_transport(key)?;
+        Ok(Swarm::new(transport, behaviour, peer_id))
+    }
+
+    #[async_std::test]
+    async fn test_sync() -> Result<()> {
+        tracing_try_init();
+        let tmp = TempDir::new("libp2p_blake_streams")?;
+        let mut server = build_swarm(&tmp.path().join("server"), [0; 32], 8192)?;
+        server.listen_on("/memory/1".parse()?)?;
+        let mut client = build_swarm(&tmp.path().join("client"), [1; 32], 8192)?;
+
+        let data = rand_bytes(1024 * 1024);
+        let mut stream = server.behaviour_mut().append(0)?;
+        stream.write_all(&data)?;
+        let head = stream.commit()?;
+
+        client.behaviour_mut().subscribe(head.head().id())?;
+        client
+            .behaviour_mut()
+            .set_peers(head.head().id(), vec![*server.local_peer_id()]);
+        client.dial_addr("/memory/1".parse().unwrap())?;
+
+        loop {
+            futures::select! {
+                ev = server.next() => {
+                    tracing::info!("server: {:?}", ev.unwrap());
+                }
+                ev = client.next() => {
+                    let ev = ev.unwrap();
+                    tracing::info!("client: {:?}", ev);
+                    match ev {
+                        SwarmEvent::ConnectionEstablished { .. } => {
+                            client.behaviour_mut().update_head(head);
+                        }
+                        SwarmEvent::Behaviour(StreamSyncEvent::NewHead(_)) => {
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+
+        let mut data2 = vec![];
+        let mut stream = client
+            .behaviour_mut()
+            .slice(head.head().id(), 0, head.head().len())?;
+        stream.read_to_end(&mut data2)?;
+        assert_eq!(data, data2);
+
+        Ok(())
     }
 }
